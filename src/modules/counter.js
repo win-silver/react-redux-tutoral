38aca098366fdf95b393e825b7c4a380464ac5a6
연승은 <seungeun_yeon@tmax.co.kr>
2021-07-19 21:38:36 +0900

    fix: counter, todos make simple using createAction, handleActions

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,31 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의. '모듈 이름 / 액션 이름'
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-// 액션 생성함수 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// 액션 생성함수
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
-  number: 0
-}
+  number: 0,
+};
 
 // 리듀서 함수
-function counter (state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number -1,
-      };
-
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 // export : 여러개 내보낼 수 있음, export default : 단 한개만 내보냄
 export default counter;
